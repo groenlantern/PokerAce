a311d0518a4221be8f150a69136b5365f801cc4d
F5156165 <jean-pierre.erasmus@fnb.co.za>
2024-03-31 00:19:25 +0200

    Update Comments

--- src/za/co/pokerface/baseCard/ShuffleDelegate.java
+++ src/za/co/pokerface/baseCard/ShuffleDelegate.java
@@ -1,6 +1,13 @@
 package za.co.pokerface.baseCard;
 import java.util.ArrayList;
 
+/**
+ * @author Jean-Pierre Erasmus
+ *
+ *
+ * Functional Interface for delegate usage
+ *
+ */
 public interface ShuffleDelegate {
 
 	ArrayList<Card> shuffleDeck(  ArrayList<Card> deck );
